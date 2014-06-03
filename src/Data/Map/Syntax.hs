dcc94ea8fe8cf2b1ec18c333390b207d343525e9
Doug Beardsley <mightybyte@gmail.com>
2014-06-03 11:16:10 -0400

    Initial commit

---
+++ src/Data/Map/Syntax.hs
@@ -0,0 +1,150 @@
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoMonomorphismRestriction  #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
+
+{-|
+
+An API implementing a convenient syntax for defining maps.  This module was
+born from the observation that a list of tuples is semantically ambiguous
+about how duplicate keys should be handled.  Additionally, the syntax is
+inherently rather cumbersome and difficult to work with.  This API takes
+advantage of do notation to provide a very light syntax for defining maps
+while at the same time eliminating the semantic ambiguity of alists.
+
+Here's an example:
+
+> foo :: MapSyntax Text
+> foo = do
+>   "firstName" ## "John"
+>   "lastName"  ## "Smith"
+
+-}
+
+module Data.Map.Syntax where
+
+
+------------------------------------------------------------------------------
+import           Control.Applicative
+import           Control.Monad.State (State, MonadState, execState, modify, put)
+import           Data.Monoid
+------------------------------------------------------------------------------
+
+
+------------------------------------------------------------------------------
+-- | Strategy to use for duplicates
+data DupStrat = Replace | Ignore | Error
+
+
+------------------------------------------------------------------------------
+-- | Representation of an indivdual item in a map
+data ItemRep k v = ItemRep
+    { irStrat :: DupStrat
+    , irKey :: k
+    , irVal :: v
+    }
+
+
+------------------------------------------------------------------------------
+type MapRep k v = [ItemRep k v]
+
+
+------------------------------------------------------------------------------
+-- | A monad providing convenient syntax for defining maps.
+newtype MapSyntaxM k v a = MapSyntaxM { unMapSyntax :: State (MapRep k v) a }
+  deriving (Functor, Applicative, Monad, MonadState (MapRep k v))
+
+
+------------------------------------------------------------------------------
+-- | Monoid instance does a union of the two maps with the second map
+-- overwriting any duplicates.
+instance Monoid (MapSyntax k v) where
+  mempty = return ()
+  mappend = (>>)
+
+
+------------------------------------------------------------------------------
+-- | Convenient type alias that will probably be used most of the time.
+type MapSyntax k v = MapSyntaxM k v ()
+
+
+------------------------------------------------------------------------------
+add :: MonadState [ItemRep k v] m => DupStrat -> k -> v -> m ()
+add strat k v = modify (++  [ItemRep strat k v])
+
+
+------------------------------------------------------------------------------
+err :: Show a => a -> b
+err k = error $ "MapSyntax: Key "++show k++" already exists in the map"
+
+
+------------------------------------------------------------------------------
+-- | Forces an entry to be added.  If the key already exists, its value is
+-- overwritten.
+(##) :: k -> v -> MapSyntax k v
+(##) = add Replace
+infixr 0 ##
+
+
+------------------------------------------------------------------------------
+-- | Tries to add an entry, but if the key already exists, then it throws an
+-- error message.  This may be useful if name collisions are bad and you want
+-- to crash when they occur.
+(#!) :: k -> v -> MapSyntax k v
+(#!) = add Error
+infixr 0 #!
+
+
+------------------------------------------------------------------------------
+-- | Inserts into the map only if the key does not already exist.
+(#?) :: k -> v -> MapSyntax k v
+(#?) = add Ignore
+infixr 0 #?
+
+
+------------------------------------------------------------------------------
+-- | Runs the MapSyntaxM monad to generate a map.
+runMapSyntax
+    :: (Monoid map, Show k)
+    => (k -> map -> Bool)
+    -- ^ Function that tests whether the key exists in the map
+    -> (k -> v -> map -> map)
+    -- ^ Function to force-insert a key-value pair into the map
+    -> MapSyntaxM k v a
+    -> map
+runMapSyntax alreadyIn forceIns ms =
+    foldl f mempty $ execState (unMapSyntax ms) mempty
+  where
+    f accum ir@ItemRep{..}
+      | irKey `alreadyIn` accum = replace accum ir
+      | otherwise = forceIns irKey irVal accum
+
+    replace accum ItemRep{..} =
+      case irStrat of
+        Replace -> forceIns irKey irVal accum
+        Ignore -> accum
+        Error -> err irKey
+
+
+------------------------------------------------------------------------------
+execMapSyntax :: MapSyntaxM k v a -> MapRep k v
+execMapSyntax ms = execState (unMapSyntax ms) mempty
+
+
+------------------------------------------------------------------------------
+-- | Maps a function over all the keys.
+mapK :: (k1 -> k2) -> MapSyntaxM k1 v a -> MapSyntax k2 v
+mapK f = MapSyntaxM . put .
+    map (\ir -> ir { irKey = f (irKey ir) }) . execMapSyntax
+
+
+------------------------------------------------------------------------------
+-- | Maps a function over all the values.
+mapV :: (v1 -> v2) -> MapSyntaxM k v1 a -> MapSyntax k v2
+mapV f = MapSyntaxM . put .
+    map (\ir -> ir { irVal = f (irVal ir) }) . execMapSyntax
+
+
